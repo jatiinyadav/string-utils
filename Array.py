e82ebc8ec81ab8f65bb22254db86d5c04e62624d
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2020-10-26 17:29:51 +0530

    Create Array.py

---
+++ Array.py
@@ -0,0 +1,16 @@
+from array import *     #here * means to include all the functions of array
+
+vals = array('i',[3,5,7,9])
+vals.reverse()
+
+for i in vals:
+    print(i)
+
+print("-------")
+
+vals = array('i',[2,5,4])
+
+for i in range(len(vals)):
+    print(vals[i])
+  
+print("-------")
