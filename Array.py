804d1cd54b9c74d6ae5a3f9f38a24bffee84e0be
Jatin Yadav <73248007+jatiinyadav@users.noreply.github.com>
2020-10-31 01:22:28 +0530

    Update Array.py

--- Array.py
+++ Array.py
@@ -19,7 +19,7 @@ val = array('i', [5,4,3,2])
 newArr = array(val.typecode, (a * a for a in val)
 print(newArr)
                
-              val = array('i',[1,3,5
+              val = array('i',[1,3,5,7,9
 
 print("-------")
                
