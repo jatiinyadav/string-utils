af01925f5de5264f9c3969d2dbeba1167fc38225
Jatin Yadav <Jatin.Yadav@wuh-group.com>
2024-06-14 01:40:39 +0530

    Minor Changes

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { capitalizeWords } from './capitalize';
+export { toCamelCase } from './camelCase';
+export { trimSpaces } from './trim';
+export { wordCount } from './count';
+export { reverseString } from './reverse';
+export { isPalindrome } from './palindrome';
