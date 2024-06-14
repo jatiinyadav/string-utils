18b9cdab5f5b1ff73e44368074dd0797f5dd7a56
Jatin Yadav <jatin27yadav@gmail.com>
2024-06-15 01:51:14 +0530

    Made it TS compatible

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-export {
-    capitalizeWords,
-    toCamelCase,
-    trimSpaces,
-    wordCount,
-    reverseString,
-    isPalindrome,
-    toDotCase,
-    toSentenceCase,
-    toKebabCase,
-    toSnakeCase
-} from './cases';
+export * from './cases';
